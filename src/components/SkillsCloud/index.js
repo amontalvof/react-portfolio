5fd064ac2e0650d787cd940bd742768cdc2581da
amontalvof <amontalvof92@gmail.com>
2020-11-17 14:54:19 -0600

    fixed render skillscloud component

--- src/components/SkillsCloud/index.js
+++ src/components/SkillsCloud/index.js
@@ -2,61 +2,64 @@ import React, { Component } from "react";
 import TagCloud from "react-tag-cloud";
 // import randomColor from "randomcolor";
 import { SkillsCloudWrapper } from "./SkillsCloudStyle";
+import { cloudSkills } from "../../context/data";
 
 class MyCloud extends Component {
-  componentDidMount() {
-    setInterval(() => {
-      this.forceUpdate();
-    }, 5000);
-  }
-  render() {
-    return (
-      <SkillsCloudWrapper>
-        <TagCloud
-          className="tag-cloud"
-          style={{
-            fontWeight: "bold",
-            padding: 10,
-            width: "100%",
-            height: "100%",
-            flex: 1,
-          }}
-        >
-          <div style={{ fontSize: 50, color: "#E54C21" }}>HTML5</div>
-          <div style={{ fontSize: 50, color: "#2862EA" }}>CSS3</div>
-          <div style={{ fontSize: 50, color: "#EFD81D" }}>JAVASCRIPT</div>
-          <div style={{ fontSize: 50, color: "#61DCFC" }}>REACT</div>
-          <div style={{ fontSize: 40, color: "#944058" }}>JEST</div>
-          <div style={{ fontSize: 40, color: "#3F2C00" }}>GIT</div>
-          <div style={{ fontSize: 30, color: "#777CB5" }}>PHP</div>
-          <div style={{ fontSize: 30, color: "#009933" }}>PYTHON</div>
-          <div style={{ fontSize: 30, color: "#336791" }}>POSTGRESQL</div>
-          <div style={{ fontSize: 30, color: "#DD8A00" }}>MYSQL</div>
-          <div style={{ fontSize: 20, color: "#B52A2A" }}>
-            MICROSOFT SQL SERVER
-          </div>
-          <div style={{ fontSize: 20, color: "#E73800" }}>CODEIGNITER</div>
-          <div style={{ fontSize: 20, color: "#553B7D" }}>BOOTSTRAP</div>
-        </TagCloud>
-        <div className="cloudmobile">
-          <div style={{ fontSize: 28, color: "#E54C21" }}>HTML5</div>
-          <div style={{ fontSize: 28, color: "#2862EA" }}>CSS3</div>
-          <div style={{ fontSize: 28, color: "#EFD81D" }}>JAVASCRIPT</div>
-          <div style={{ fontSize: 28, color: "#61DCFC" }}>REACT</div>
-          <div style={{ fontSize: 24, color: "#944058" }}>JEST</div>
-          <div style={{ fontSize: 24, color: "#3F2C00" }}>GIT</div>
-          <div style={{ fontSize: 20, color: "#777CB5" }}>PHP</div>
-          <div style={{ fontSize: 20, color: "#009933" }}>PYTHON</div>
-          <div style={{ fontSize: 20, color: "#336791" }}>POSTGRESQL</div>
-          <div style={{ fontSize: 20, color: "#DD8A00" }}>MYSQL</div>
-          <div style={{ fontSize: 16, color: "#B52A2A" }}>
-            MICROSOFT SQL SERVER
-          </div>
-          <div style={{ fontSize: 16, color: "#E73800" }}>CODEIGNITER</div>
-          <div style={{ fontSize: 16, color: "#553B7D" }}>BOOTSTRAP</div>
-        </div>
-      </SkillsCloudWrapper>
-    );
-  }
+    componentDidMount() {
+        setInterval(() => {
+            this.forceUpdate();
+        }, 5000);
+    }
+    render() {
+        return (
+            <SkillsCloudWrapper>
+                <TagCloud
+                    className="tag-cloud"
+                    style={{
+                        fontWeight: "bold",
+                        padding: 10,
+                        width: "100%",
+                        height: "100%",
+                        flex: 1,
+                    }}
+                >
+                    {cloudSkills.map((item) => {
+                        const {
+                            id,
+                            technology,
+                            color,
+                            fontSize: { web },
+                        } = item;
+                        return (
+                            <div
+                                key={id}
+                                style={{ fontSize: web, color: color }}
+                            >
+                                {technology}
+                            </div>
+                        );
+                    })}
+                </TagCloud>
+                <div className="cloudmobile">
+                    {cloudSkills.map((item) => {
+                        const {
+                            id,
+                            technology,
+                            color,
+                            fontSize: { mobile },
+                        } = item;
+                        return (
+                            <div
+                                key={id}
+                                style={{ fontSize: mobile, color: color }}
+                            >
+                                {technology}
+                            </div>
+                        );
+                    })}
+                </div>
+            </SkillsCloudWrapper>
+        );
+    }
 }
 export default MyCloud;
--- src/context/data.js
+++ src/context/data.js
@@ -1,131 +1,253 @@
 export const employment = [
-  {
-    id: 1,
-    title: "Web Developer and Database Administrator",
-    place: "ETECSA- Empresa de Telecomunicaciones de Cuba S.A. Havana, Cuba",
-    date: "06/2017 - 12/2018",
-    description:
-      "Specialized in web development and database administration using PHP/CodeIgniter, Microsoft SQL Server, MySQL, and PostgreSQL. Conducted quality assurance tests to look for errors, developing and implementing effective solutions to the issues. Worked on back-end development tasks, and performed regular maintenance, troubleshooting, system restores, and system backups when necessary. Oversaw database system development to ensure requirements were met and modifications were made when necessary.",
-  },
-  {
-    id: 2,
-    title: "Front-end Programmer",
-    place: "Ministerio del Transporte. Havana, Cuba",
-    date: "09/2016 – 06/2017",
-    description:
-      "Specialized in HTML5, CSS3, and JavaScript. Framework: Bootstrap. Collaborated with designers and other team members to create clean interfaces and to optimize the experience and interactions of the user. Completed front-end development tasks and ensured user experience design was incorporated into websites for optimal user accessibility. Remained up to date on current development practices and standards to produce only the latest and highest-quality work.",
-  },
+    {
+        id: 1,
+        title: "Web Developer and Database Administrator",
+        place:
+            "ETECSA- Empresa de Telecomunicaciones de Cuba S.A. Havana, Cuba",
+        date: "06/2017 - 12/2018",
+        description:
+            "Specialized in web development and database administration using PHP/CodeIgniter, Microsoft SQL Server, MySQL, and PostgreSQL. Conducted quality assurance tests to look for errors, developing and implementing effective solutions to the issues. Worked on back-end development tasks, and performed regular maintenance, troubleshooting, system restores, and system backups when necessary. Oversaw database system development to ensure requirements were met and modifications were made when necessary.",
+    },
+    {
+        id: 2,
+        title: "Front-end Programmer",
+        place: "Ministerio del Transporte. Havana, Cuba",
+        date: "09/2016 – 06/2017",
+        description:
+            "Specialized in HTML5, CSS3, and JavaScript. Framework: Bootstrap. Collaborated with designers and other team members to create clean interfaces and to optimize the experience and interactions of the user. Completed front-end development tasks and ensured user experience design was incorporated into websites for optimal user accessibility. Remained up to date on current development practices and standards to produce only the latest and highest-quality work.",
+    },
 ];
 
 export const education = [
-  {
-    id: 1,
-    title: "Master of Science in Electronics Engineering",
-    place: "Technological Institute of Celaya. Celaya, Mexico",
-    date: "01/2019 - 01/2021",
-    description:
-      "Fields of study: Electronics, Deep Learning and Computer Vision using Python.",
-  },
-  {
-    id: 2,
-    title: "Telecommunication and Electronics Engineer",
-    place:
-      "José Antonio Echeverría Technological University of Havana-Cujae. Havana, Cuba",
-    date: "09/2011 - 06/2016",
-    description:
-      "Thesis: “Vibration measurement for leak detection in water distribution networksˮ. Focus on digital signal processing using Python.",
-  },
+    {
+        id: 1,
+        title: "Master of Science in Electronics Engineering",
+        place: "Technological Institute of Celaya. Celaya, Mexico",
+        date: "01/2019 - 01/2021",
+        description:
+            "Fields of study: Electronics, Deep Learning and Computer Vision using Python.",
+    },
+    {
+        id: 2,
+        title: "Telecommunication and Electronics Engineer",
+        place:
+            "José Antonio Echeverría Technological University of Havana-Cujae. Havana, Cuba",
+        date: "09/2011 - 06/2016",
+        description:
+            "Thesis: “Vibration measurement for leak detection in water distribution networksˮ. Focus on digital signal processing using Python.",
+    },
 ];
 
 export const leng = [
-  {
-    id: 1,
-    title: "HTML5",
-    porciento: 90,
-  },
-  {
-    id: 2,
-    title: "CSS3",
-    porciento: 80,
-  },
-  {
-    id: 3,
-    title: "JAVASCRIPT",
-    porciento: 80,
-  },
-  {
-    id: 4,
-    title: "PHP",
-    porciento: 70,
-  },
-  {
-    id: 5,
-    title: "PYTHON",
-    porciento: 70,
-  },
+    {
+        id: 1,
+        title: "HTML5",
+        porciento: 90,
+    },
+    {
+        id: 2,
+        title: "CSS3",
+        porciento: 80,
+    },
+    {
+        id: 3,
+        title: "JAVASCRIPT",
+        porciento: 80,
+    },
+    {
+        id: 4,
+        title: "PHP",
+        porciento: 70,
+    },
+    {
+        id: 5,
+        title: "PYTHON",
+        porciento: 70,
+    },
 ];
 
 export const fram = [
-  {
-    id: 6,
-    title: "SQL",
-    porciento: 70,
-  },
-  {
-    id: 7,
-    title: "BOOTSTRAP",
-    porciento: 80,
-  },
-  {
-    id: 8,
-    title: "REACT",
-    porciento: 80,
-  },
-  {
-    id: 9,
-    title: "CODEIGNITER",
-    porciento: 70,
-  },
-  {
-    id: 10,
-    title: "GITHUB",
-    porciento: 90,
-  },
+    {
+        id: 6,
+        title: "SQL",
+        porciento: 70,
+    },
+    {
+        id: 7,
+        title: "BOOTSTRAP",
+        porciento: 80,
+    },
+    {
+        id: 8,
+        title: "REACT",
+        porciento: 80,
+    },
+    {
+        id: 9,
+        title: "CODEIGNITER",
+        porciento: 70,
+    },
+    {
+        id: 10,
+        title: "GITHUB",
+        porciento: 90,
+    },
 ];
 
 export const proj = [
-  {
-    id: 1,
-    type: "main",
-    tech:
-      "React Rainbow Components is an open-source project, with a collection of components that will help you build your application in a faster way. I am one of the collaborators.",
-    alt: "React Rainbow Components",
-    img: "./img/P_RRC.jpg",
-    url: "https://react-rainbow.io/#/GettingStarted",
-  },
-  {
-    id: 2,
-    type: "not for now",
-    tech: "React, Styled Components, Jest, Enzyme, Cypress, Firebase ...",
-    alt: "Rainbow Modules",
-    img: "./img/P_RM.jpg",
-    url:
-      "https://rainbow-modules.web.app/?path=/story/introduction-getting-started-overview--page",
-  },
-  {
-    id: 3,
-    type: "practice",
-    tech: "React, Styled Components",
-    alt: "Antique Tech Store",
-    img: "./img/P_ATS.jpg",
-    url: "https://am-react-antique-tech-store.netlify.app",
-  },
-  {
-    id: 4,
-    type: "practice",
-    tech: "React, Styled Components",
-    alt: "Technology Store",
-    img: "./img/P_TS.jpg",
-    url: "https://am-react-technology-store.netlify.app",
-  },
+    {
+        id: 1,
+        type: "main",
+        tech:
+            "React Rainbow Components is an open-source project, with a collection of components that will help you build your application in a faster way. I am one of the collaborators.",
+        alt: "React Rainbow Components",
+        img: "./img/P_RRC.jpg",
+        url: "https://react-rainbow.io/#/GettingStarted",
+    },
+    {
+        id: 2,
+        type: "main",
+        tech:
+            "Rainbow Modules includes a collection of components, fully integrated with Firebase, that will help you to build apps. I am one of the collaborators.",
+        alt: "Rainbow Modules",
+        img: "./img/P_RM.jpg",
+        url:
+            "https://rainbow-modules.web.app/?path=/story/introduction-getting-started-overview--page",
+    },
+    {
+        id: 3,
+        type: "practice",
+        tech: "React, Styled Components",
+        alt: "Antique Tech Store",
+        img: "./img/P_ATS.jpg",
+        url: "https://am-react-antique-tech-store.netlify.app",
+    },
+    {
+        id: 4,
+        type: "practice",
+        tech: "React, Styled Components",
+        alt: "Technology Store",
+        img: "./img/P_TS.jpg",
+        url: "https://am-react-technology-store.netlify.app",
+    },
+];
+
+export const cloudSkills = [
+    {
+        id: 1,
+        technology: "HTML5",
+        fontSize: {
+            web: 50,
+            mobile: 28,
+        },
+        color: "#E54C21",
+    },
+    {
+        id: 2,
+        technology: "CSS3",
+        fontSize: {
+            web: 50,
+            mobile: 28,
+        },
+        color: "#2862EA",
+    },
+    {
+        id: 3,
+        technology: "JAVASCRIPT",
+        fontSize: {
+            web: 50,
+            mobile: 28,
+        },
+        color: "#EFD81D",
+    },
+    {
+        id: 4,
+        technology: "REACT",
+        fontSize: {
+            web: 50,
+            mobile: 28,
+        },
+        color: "#61DCFC",
+    },
+    {
+        id: 6,
+        technology: "JEST",
+        fontSize: {
+            web: 40,
+            mobile: 24,
+        },
+        color: "#944058",
+    },
+    {
+        id: 7,
+        technology: "GIT & GITHUB",
+        fontSize: {
+            web: 40,
+            mobile: 24,
+        },
+        color: "#3F2C00",
+    },
+    {
+        id: 8,
+        technology: "PHP",
+        fontSize: {
+            web: 30,
+            mobile: 20,
+        },
+        color: "#777CB5",
+    },
+    {
+        id: 9,
+        technology: "PYTHON",
+        fontSize: {
+            web: 30,
+            mobile: 20,
+        },
+        color: "#009933",
+    },
+    {
+        id: 10,
+        technology: "POSTGRESQL",
+        fontSize: {
+            web: 30,
+            mobile: 20,
+        },
+        color: "#336791",
+    },
+    {
+        id: 11,
+        technology: "MYSQL",
+        fontSize: {
+            web: 30,
+            mobile: 20,
+        },
+        color: "#DD8A00",
+    },
+    {
+        id: 11,
+        technology: "MICROSOFT SQL SERVER",
+        fontSize: {
+            web: 20,
+            mobile: 16,
+        },
+        color: "#B52A2A",
+    },
+    {
+        id: 12,
+        technology: "CODEIGNITER",
+        fontSize: {
+            web: 20,
+            mobile: 16,
+        },
+        color: "#E73800",
+    },
+    {
+        id: 12,
+        technology: "BOOTSTRAP",
+        fontSize: {
+            web: 20,
+            mobile: 16,
+        },
+        color: "#553B7D",
+    },
 ];
