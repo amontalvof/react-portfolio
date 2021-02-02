d35bd7c56a4e955e5e2b7605bcb0bc7764c98152
amontalvof <amontalvof92@gmail.com>
2021-02-02 11:23:41 -0600

    added dev to contact icons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,42 +1,46 @@
-import React from "react";
-import { Element } from "react-scroll";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import Title from "../Title";
-import { ContactWrapper } from "./ContactStyle";
+import React from 'react';
+import { Element } from 'react-scroll';
+import { FaLinkedin, FaGithub, FaDev } from 'react-icons/fa';
+import Title from '../Title';
+import { ContactWrapper } from './ContactStyle';
 
 export default function index() {
-  return (
-    <ContactWrapper>
-      <Element name="contact">
-        <Title title="Contact" white={true} />
-      </Element>
-      <br />
-      <div>
-        <a
-          className="iconosSociales"
-          href={
-            "https://www.linkedin.com/in/andy-montalvo-fern%C3%A1ndez-6a9a37148/"
-          }
-        >
-          <FaLinkedin />
-        </a>
-        <a className="iconosSociales" href={"https://github.com/amontalvof"}>
-          <FaGithub />
-        </a>
-      </div>
-      <div>
-        <h3>Phone: +52 461 253 4450</h3>
-      </div>
-      <div>
-        <h3>Email: andy.montalvowd@gmail.com</h3>
-      </div>
-      <div>
-        <p>
-          Copyright &copy; Andy Montalvo {new Date().getFullYear()}, all rights
-          reserved.
-        </p>
-        <br />
-      </div>
-    </ContactWrapper>
-  );
+    return (
+        <ContactWrapper>
+            <Element name="contact">
+                <Title title="Contact" white={true} />
+            </Element>
+            <br />
+            <div>
+                <a
+                    className="iconosSociales"
+                    href="https://www.linkedin.com/in/andy-montalvo-fern%C3%A1ndez-6a9a37148/"
+                >
+                    <FaLinkedin />
+                </a>
+                <a
+                    className="iconosSociales"
+                    href="https://github.com/amontalvof"
+                >
+                    <FaGithub />
+                </a>
+                <a className="iconosSociales" href="https://dev.to/amontalvof">
+                    <FaDev />
+                </a>
+            </div>
+            <div>
+                <h3>Phone: +52 461 253 4450</h3>
+            </div>
+            <div>
+                <h3>Email: andy.montalvowd@gmail.com</h3>
+            </div>
+            <div>
+                <p>
+                    Copyright &copy; Andy Montalvo {new Date().getFullYear()},
+                    all rights reserved.
+                </p>
+                <br />
+            </div>
+        </ContactWrapper>
+    );
 }
